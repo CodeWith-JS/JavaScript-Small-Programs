37759070cd3bf7b1122a0956c7be19aca21e7eeb
CodeWithJS <junedsheikh0202@gmail.com>
2026-09-01 09:54:32 +0530

    Datatypes

---
+++ datatypes.js
@@ -0,0 +1,28 @@
+// in this file there is info about datatypes in js 
+
+//string
+let color ="green";
+let lastname ="man";
+
+//NUmber 
+let count = 10;
+let total = 100;
+
+//bigint
+let bignum = 1000266526632682365653n;
+let x = BigInt(65565125346)
+
+//boolean 
+let yes = true;
+let no = false;
+
+//object
+const person = {firstname : "juned", lastname: "shekh"};
+
+//arrey object
+const car =["porce","bugati","ferari","supera"];
+
+console.log(color,lastname,count,total,bignum,x);
+console.log(yes,no);
+console.log(person);
+console.log(car);
