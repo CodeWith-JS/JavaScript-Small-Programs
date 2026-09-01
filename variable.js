d4285b2098cd44796df8f7bb9f75205612fa0657
CodeWithJS <junedsheikh0202@gmail.com>
2026-09-01 09:54:09 +0530

    variables

---
+++ variable.js
@@ -0,0 +1,16 @@
+
+//this are global  and function scope 
+var Name = "juned"
+var userid = 1;
+
+//this are block scope 
+let Username = "juned";
+const userID = 2;
+
+
+console.log(Name, userid);
+console.log(Username,userID);
+
+//this is hoisting 
+console.log(user);
+var user = "Juned";    
