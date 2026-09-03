5c76aeecd97e9bfa8968e47f7459882258b13888
CodeWithJS <junedsheikh0202@gmail.com>
2026-09-03 12:58:29 +0530

    Operators

---
+++ operators.js
@@ -0,0 +1,41 @@
+// total types of operator in js 
+// arithmetic 
+// comparision 
+// assignment 
+// logical 
+// uniery 
+// ternory 
+
+// console.log(5+6);
+// console.log(5-6);
+// console.log(5*6);
+// console.log(5/6);
+
+let a = 8;
+let b = 7;
+
+// console.log(c);
+
+// assignment opretor 
+// console.log(a += b);
+// console.log(a -= b);
+// console.log(a *= b);
+// console.log(a /= b);
+
+
+//comparision oprator 
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+
+//logical operators
+// console.log(a == b && a === b);
+// console.log(a == b || a < b );
+
+//unary operator 
+let c = 5;
+console.log(c++);//post increment 
+console.log(++c);//pre increment
