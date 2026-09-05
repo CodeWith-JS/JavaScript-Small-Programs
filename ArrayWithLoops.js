0f86ef26d4212434f45224f001f313d09d30e92b
CodeWithJS <junedsheikh0202@gmail.com>
2026-09-05 10:53:48 +0530

    ArrayWithLoops

---
+++ ArrayWithLoops.js
@@ -0,0 +1,24 @@
+//let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+let arr = ["yt", "channel", "Codewith js"];
+
+//let len = arr.length;
+let i = arr;
+for ( i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+// let i = 0;
+// while (i < arr.length) {
+//     console.log(arr[i]);
+//     i++;
+// }
+
+//ForOF
+// for (let val of arr) {
+//     console.log(val);
+// }
+
+// arr.forEach(function (val) {
+//     console.log(val);
+// });
